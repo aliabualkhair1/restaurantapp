80a23bedbbef9d27698536d65e63e658c1feb342
aliabualkhair1 <156579098+aliabualkhair1@users.noreply.github.com>
2025-11-25 19:05:25 +0200

    Update orderitems.ts

--- src/app/Components/Models/orderitems/orderitems.ts
+++ src/app/Components/Models/orderitems/orderitems.ts
@@ -4,6 +4,7 @@ import { Dashboardservice } from '../../../Services/dashboardservice';
 import { ApiResponse, Orderitem } from '../../../Interfaces/Models/orderitem';
 import { Spinner } from '../spinner/spinner';
 import { CommonModule } from '@angular/common';
+import { Order } from '../../../Interfaces/Models/order';
 @Component({
   selector: 'app-orderitems',
   imports: [CommonModule, Spinner],
@@ -14,23 +15,38 @@ export class Orderitems implements OnInit {
   id:any
   orderitem:Orderitem[]=[]
   loading:boolean=false
+  apiMessage: string = '';
+  apiMessageType: 'success' | 'error' | '' = '';
 constructor(private http:Dashboardservice,private routing:ActivatedRoute){
 this.id=this.routing.snapshot.paramMap.get('id')
 }
   ngOnInit(): void {
 this.getorderitemsbyorderid()
   }
-getorderitemsbyorderid(){
-  this.loading=true
-  this.http.getorderitemsbyorderid(this.id).subscribe({
-    next:(res:ApiResponse)=>{
-  this.orderitem=res.orderItems
-  this.loading=false
-    },
-    error:(err)=>{
-      alert("an error occured")
-      this.loading=false
-    }
-  })
-}
+
+  getorderitemsbyorderid() {
+    this.loading = true;
+    this.http.getorderitemsbyorderid(this.id).subscribe({
+      next: (res: Order[] | any) => {
+        if (res && res.length > 0) {
+          this.orderitem = res[0].orderItems;
+        } else {
+          this.orderitem = [];
+        }
+        this.loading = false;
+      },
+      error: (err) => {
+        this.showMessage(err.error || "An error occurred", 'error');
+        this.loading = false;
+      }
+    });
+  }
+    private showMessage(message: string, type: 'success' | 'error') {
+    this.apiMessage = message;
+    this.apiMessageType = type;
+    setTimeout(() => {
+      this.apiMessage = '';
+      this.apiMessageType = '';
+    }, 5000);
+  }
 }
