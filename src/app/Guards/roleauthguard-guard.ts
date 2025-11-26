7fd0adc30ff0a2b6891ee642556a4fe748464fea
aliabualkhair1 <156579098+aliabualkhair1@users.noreply.github.com>
2025-11-26 16:46:19 +0200

    Update roleauthguard-guard.ts

--- src/app/Guards/roleauthguard-guard.ts
+++ src/app/Guards/roleauthguard-guard.ts
@@ -1,22 +1,17 @@
-import { inject } from "@angular/core";
-import { CanActivateFn, Router } from "@angular/router";
-import { Roles } from "../Services/roles";
-
 export const roleauthguardGuard: CanActivateFn = (route, state) => {
+
   const roles = inject(Roles);
   const router = inject(Router);
 
   const allowedRoles = route.data['roles'] as string[];
-  const userRole = roles.getrole();
-  if (!localStorage.getItem("refresh token")) {
+  if (!roles.IsAuthenticated()) {
     router.navigate(['/login']);
     return false;
   }
-
+  const userRole = roles.getrole();
   if (userRole && allowedRoles.includes(userRole)) {
     return true;
   }
-
   router.navigate(['/home']);
   return false;
 };
